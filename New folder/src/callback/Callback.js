0b2fba88fca9c715a8330a9595f63ef3f79d944c
tbm02 <tarun989898@gmail.com>
2023-03-19 00:58:47 +0530

    [Callback Solution]

--- New folder/src/callback/Callback.js
+++ New folder/src/callback/Callback.js
@@ -5,9 +5,19 @@ function App() {
   const [number, setnumber] = useState(1);
   const [dark, setdark] = useState(false);
 
-
+  
   const getItems = useCallback(() => {
-    return [number, number + 1, number + 2]
+    // Doing some extra work here
+    // This Function will get trigeerd on the change of number value 
+    // It will only get triggered on the change to the number
+    
+    for(let i = 1;i<1000_000;i++){
+      if(i%100000 == 0){
+        // console.log("doing some extra work")
+        console.log("Notice how this method only got called as the valuee changed")
+      }
+    }
+    return [number,number+1,number+2]
   },[number]);
   // useEffect(()=>{
   //   console.log("number render")
@@ -23,7 +33,9 @@ function App() {
   // },[theme])
 
   return (
+    
     <>
+    {console.log("Render method is called ... Inside the component where callbacki is called yet it didnt trigger the function isnide the usecall back  ")}
     <h1>callback</h1>
     <div style ={theme}>
       <input
@@ -31,7 +43,10 @@ function App() {
         value={number}
         onChange={e => setnumber(parseInt(e.target.value))}
       />
-      <button onClick={() => setdark(prevdark => !prevdark)}>
+      <button onClick={() => setdark(prevdark => {
+        console.log("Toggling the button will triger the render")
+        
+        return !prevdark})}>
         Toggle button
       </button>
       {/* <div style={theme}/> */}
